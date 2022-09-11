d67114e7668473aa1ab95718e7f0e89640f78b9e
farhad.hassani <farhad.hassani@relexsolutions.com>
2022-09-11 16:05:26 +0300

    remove LSP_status

--- lua/nvchad_ui/statusline/init.lua
+++ lua/nvchad_ui/statusline/init.lua
@@ -11,14 +11,14 @@ return {
     return table.concat {
       modules.mode(),
       modules.fileInfo(),
-      modules.git(),
 
       "%=",
       modules.LSP_progress(),
       "%=",
 
       modules.LSP_Diagnostics(),
-      modules.LSP_status() or "",
+      modules.git(),
+      -- modules.LSP_status() or "",
       modules.cwd(),
       -- modules.cursor_position(),
     }
